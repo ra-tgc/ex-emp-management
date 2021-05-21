05bacf4963a5ebae548ac5430d5d7e5c0be6d83e
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-05-21 12:33:48 +0900

    AdministratorControllerにinsertメソッドを追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,12 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -38,6 +40,22 @@ public class AdministratorController {
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
+	}
+
+	/**
+	 * 管理者情報を登録する.
+	 * 
+	 * @param form 管理者情報のフォーム
+	 * @return "/"(ログイン画面)へのリダイレクト
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator admin = new Administrator();
+		BeanUtils.copyProperties(form, admin);
+		administratorService.insert(admin);
+
+		return "redirect:/";
+
 	}
 
 	@RequestMapping("/test-admin")
