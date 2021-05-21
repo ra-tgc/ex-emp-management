7b75d135d267b82820a7fb10b8990117d0f7efdb
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-05-21 15:16:22 +0900

    EmployeeControllerにshowDetailメソッドを追加
    detail.htmlを追加
    
    細かなバグを修正

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -23,6 +25,16 @@ public class EmployeeController {
 	private EmployeeService employeeService;
 
 	/**
+	 * UpdateEmployeeFormをインスタンス化する.
+	 * 
+	 * @return インスタンス化されたUpdateEmployeeForm
+	 */
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+
+	/**
 	 * 従業員一覧を出力する.
 	 * 
 	 * @param model requestスコープ
@@ -34,6 +46,21 @@ public class EmployeeController {
 		model.addAttribute("employeeList", employeeList);
 
 		return "employee/list";
+	}
+
+	/**
+	 * 従業員の詳細情報を出力する.
+	 * 
+	 * @param id    主キー(リクエストパラメータで送られてくる)
+	 * @param model requestスコープ
+	 * @return employee/detail.htmlのビュー
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+
+		return "employee/detail.html";
 	}
 
 //	デバッグ用
