89e882bf5fd6657dd65284f21602526bf764ed24
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-05-21 14:05:45 +0900

    AdministratorControllerにloginメソッドを追加
    loginビューをエラーチェックを行うように修正
    
    細かなバグを修正

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +25,10 @@ import jp.co.sample.service.AdministratorService;
 public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
+
+	/** セッションスコープ */
+	@Autowired
+	private HttpSession session;
 
 	/**
 	 * InsertAdministratorFormをインスタンス化する.
@@ -78,11 +85,26 @@ public class AdministratorController {
 		return "redirect:/";
 	}
 
-	@RequestMapping("/test-admin")
-	public String testAdmin() {
+	@RequestMapping("/login")
+	public String login(LoginForm form, Model model) {
+		Administrator admin = administratorService.login(form.getMailAddress(), form.getPassword());
 
-		administratorService.testRepository();
+		if (admin == null) {
+			model.addAttribute("error", "メールアドレスまたはパスワードが不正です");
+			return "administrator/login";
+		}
 
-		return "finished";
+		session.setAttribute("administratorName", admin);
+
+		return "/employee/showList";
 	}
+
+// デバッグ用
+//	@RequestMapping("/test-admin")
+//	public String testAdmin() {
+//
+//		administratorService.testRepository();
+//
+//		return "finished";
+//	}
 }
